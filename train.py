9c357d9308cde9b135243f9d80f0c8d0818291c6
Spaak <spaakjobrighngoma@gmail.com>
2023-10-19 01:56:44 +0100

    add lstm model

--- train.py
+++ train.py
@@ -1,1 +1,6 @@
-print("Hello")
+package tn.insat.tp1;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+import java.io.IOException;
+import java.util.StringTokenizer;
